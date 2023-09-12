b18d910219876a71b2821be950ca33406e2c3a66
Ramozzzzz <937816712@qq.com>
2023-09-12 20:55:43 +0800

    1

--- src/com/Words/txtOperation.java
+++ src/com/Words/txtOperation.java
@@ -2,6 +2,7 @@ package com.Words;
 
 import java.io.*;
 import java.util.*;
+import java.util.stream.Collectors;
 
 import static com.Words.WordsCut.*;
 
@@ -19,8 +20,18 @@ class txtOperation {
         return article;
     }
 
+    private static List<String> Merge(List<String> s1,List<String> s2){
+        List<String> str1=new ArrayList<>();
+        List<String> str2=new ArrayList<>();
+        str1.addAll(s1);
+        str1.addAll(s2);
+        str2=str1.stream().distinct().collect(Collectors.toList());
+        return str2;
+    }
+
     public static void main(String[] args) throws IOException {
-        String path="F:\\program\\c\\software\\txt\\orig.txt";
-        System.out.println(splitWords(String.valueOf(Read(path))));
+//        String path="F:\\program\\c\\software\\txt\\orig.txt";
+//        System.out.println(splitWords(String.valueOf(Read(path))));
+
     }
 }
