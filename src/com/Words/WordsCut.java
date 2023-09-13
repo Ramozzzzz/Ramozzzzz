82bcd31cfd872279f403c84ed14ee6334c5277a9
Ramo <937816712@qq.com>
2023-09-13 15:46:13 +0800

    新增函数

--- src/com/Words/WordsCut.java
+++ src/com/Words/WordsCut.java
@@ -12,4 +12,13 @@ public class WordsCut {
         return s2.filter(s->!"`~!@#$^&*()=|{}':;',\\[\\].<>/?~！@#￥……&*（）——|{}【】‘；：”“'。，、？ "
                 .contains(s)).toList();
     }
+
+    private static List<String> Merge(List<String> s1,List<String> s2){
+        List<String> str1=new ArrayList<>();
+        List<String> str2=new ArrayList<>();
+        str1.addAll(s1);
+        str1.addAll(s2);
+        str2=str1.stream().distinct().collect(Collectors.toList());
+        return str2;
+    }
 }
--- src/com/Words/txtOperation.java
+++ src/com/Words/txtOperation.java
@@ -20,14 +20,7 @@ class txtOperation {
         return article;
     }
 
-    private static List<String> Merge(List<String> s1,List<String> s2){
-        List<String> str1=new ArrayList<>();
-        List<String> str2=new ArrayList<>();
-        str1.addAll(s1);
-        str1.addAll(s2);
-        str2=str1.stream().distinct().collect(Collectors.toList());
-        return str2;
-    }
+
 
     public static void main(String[] args) throws IOException {
 //        String path="F:\\program\\c\\software\\txt\\orig.txt";
