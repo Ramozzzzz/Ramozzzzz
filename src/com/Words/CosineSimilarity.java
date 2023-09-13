2f01f9630899a14895b0774b36c0ff14c106dda7
Ramo <937816712@qq.com>
2023-09-13 17:30:49 +0800

    编写余弦相似度查重方法

--- src/com/Words/CosineSimilarity.java
+++ src/com/Words/CosineSimilarity.java
@@ -1,7 +1,7 @@
 package com.Words;
 
 public class CosineSimilarity {
-    static float getSimilarity(int[] number1,int[] number2){
+    public static float getSimilarity(int[] number1,int[] number2){
         float square1=0,square2=0,product=0;
         for(int i=0;i<number1.length;i++){
             product+=number1[i]*number2[i];
