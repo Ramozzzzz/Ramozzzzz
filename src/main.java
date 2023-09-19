2177a4611fc69627d7f0cb04792b6d0045308238
Ramo <937816712@qq.com>
2023-09-19 18:04:30 +0800

    主类

---
+++ src/main.java
@@ -0,0 +1,7 @@
+import java.io.IOException;
+
+public class main {
+    public static void main(String[] args) throws IOException {
+        System.out.println(PostfixExpression.postfixChange("2 * ( 2 + 2 )"));
+    }
+}
