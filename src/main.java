af1462992dbe8e65021b047cc630d59ac327d243
Ramo <937816712@qq.com>
2023-09-20 20:27:31 +0800

    临时测试

--- src/main.java
+++ src/main.java
@@ -1,7 +1,13 @@
-import java.io.IOException;
 
 public class main {
-    public static void main(String[] args) throws IOException {
-        System.out.println(PostfixExpression.postfixChange("2 * ( 2 + 2 )"));
+    public static void main(String[] args){
+        System.out.println(PostfixExpression.postfixChange("2 ' 1 / 4 + 2 ' 1 / 4"));
+
+        System.out.println(Calculator.fractionCalculator(PostfixExpression.postfixChange("2 ' 1 / 4 + 2 ' 1 / 4")));
+//        Fraction f1=new Fraction(5,3);
+//        Fraction f2=new Fraction(6,1);
+//        f1=f1.subFraction(f2);
+//        System.out.println(f1.denominator);
+//        System.out.println(f1.numerator);
     }
 }
